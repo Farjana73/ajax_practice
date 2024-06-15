abf31503b5a27d8baba2c64b0bcf7cbfd6479447
Farjana73 <faakther73@gmail.com>
2024-06-15 08:09:05 +0600

    added new file

---
+++ js/script2.js
@@ -0,0 +1,15 @@
+// async function CallMyData(){
+  
+// let data = await fetch ('https://jsonplaceholder.typicode.com/albums');
+
+// let JSONdata = await data.json();
+// console.log(JSONdata)
+
+// }
+
+// CallMyData();
+
+async function foo() {
+    return await 'Hello';
+  }
+  console.log(foo());
